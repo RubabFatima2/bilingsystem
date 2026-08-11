7b35829d05ab169236149fd52efd8029daa70fb9
RubabFatima2 <fatimarubab6612@gmail.com>
2026-08-11 11:03:47 +0500

    feat: add invoice repository

---
+++ app/repositories/invoice_repository.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+
+from app.models.invoice import Invoice
+
+
+class InvoiceRepository:
+
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create(self, invoice: Invoice):
+        self.db.add(invoice)
+        self.db.commit()
+        self.db.refresh(invoice)
+
+        return invoice
